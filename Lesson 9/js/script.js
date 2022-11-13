44277579efeea68cbb1fd108db3f0a08efe0d0b3
Ruslan Yarema <yarema2003r@gmail.com>
2022-11-13 20:13:27 +0200

    Lesson 9 start

---
+++ Lesson 9/js/script.js
@@ -0,0 +1,31 @@
+
+class Button {
+    constructor(width, height, content)
+    {
+        this.width = width;
+        this.height = height;
+        this.content = content;
+    }
+
+    showBtn() {
+        document.write(`<button style="width: ${this.width}px; height: ${this.height}px;">${this.content}</button>`);
+    }
+}
+class BootstrapButton extends Button {
+    constructor(width, height, content, color)
+    {
+        super(width, height, content)
+        this.color = color;
+    }
+
+    showBtn() {
+        document.write(`<button style="width: ${this.width}px; height: ${this.height}px; color: ${this.color}">${this.content}</button>`);
+    }
+}
+
+let width = +prompt('width')
+let height = +prompt('height')
+let content = +prompt('content')
+let color = +prompt('color')
+
+new BootstrapButton(width, height, content, color).showBtn();
